83a2eb9dfe783cea74dc1f4d189af32f1a125a7c
Ibrahim Zeroual <izeroual@novelis.io>
2023-08-13 15:27:52 +0100

    adding testContainers lib and testing post and getAll endpoints

--- src/test/java/com/learning/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/learning/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,66 @@
 package com.learning.productservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learning.productservice.dto.ProductDto;
+import com.learning.productservice.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
     @Test
-    void contextLoads() {
+    void shouldCreateProduct() throws Exception {
+
+        String productDto = objectMapper.writeValueAsString(getProductDto());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/products")
+                .contentType("application/json")
+                .content(productDto)
+        ).andExpect(status().isOk());
+
+    }
+    @Test
+    void productsSize(){
+        Assertions.assertEquals(1,productRepository.findAll().size());
+    }
+
+    private ProductDto getProductDto() {
+        return ProductDto.builder().name("pc").description("really bad")
+                .price(BigDecimal.valueOf(340)).build();
     }
 
 }
